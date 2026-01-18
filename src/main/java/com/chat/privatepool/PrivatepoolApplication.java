99c024425f84fbe72a04040d4b0811f006554be6
DeepakAd1 <deepak.kumar@kapturecrm.com>
2026-01-18 14:11:37 +0530

    Clean initial commit

---
+++ src/main/java/com/chat/privatepool/PrivatepoolApplication.java
@@ -0,0 +1,13 @@
+package com.chat.privatepool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatepoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivatepoolApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/chat/privatepool/configration/WebSocketConfig.java
@@ -0,0 +1,10 @@
+package com.chat.privatepool.configration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@EnableWebSocket
+@Configuration
+public class WebSocketConfig {
+    
+}
